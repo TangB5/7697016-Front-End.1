eadc46d6226d5748e082ea43121e41c5f31621b5
TangB5 <122724404+TangB5@users.noreply.github.com>
2025-06-05 15:43:49 +0200

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -1,19 +1,45 @@
-// Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('pieces-autos.json');
+const reponse= await fetch('../pieces-autos.json');
 const pieces = await reponse.json();
-// Création des balises 
-const article = pieces[0];
-const imageElement = document.createElement("img");
+
+for (let i = 0; i < pieces.length ; i++) {
+
+
+const article = pieces[i];
+const imageElement=document.createElement('img');
 imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-//Rattachement de nos balises au DOM
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
+
+const titleElement=document.createElement('div');
+
+titleElement.classList.add('font-bold', 'text-lg', 'mb-2');
+titleElement.innerText = article.nom;
+
+const prixElement=document.createElement('div');
+prixElement.classList.add('text-gray-600', 'mb-2');
+prixElement.innerHTML = `Prix: ${article.prix}  ${article.prix < 35 ? '<span class="text-green-500 font-extrabold">fcfa</span>' : '<span class="text-red-500 font-extrabold">fcfa</span>'}`;
+
+const categorieElement=document.createElement('div');
+categorieElement.classList.add('text-gray-600', 'mb-2');
+categorieElement.innerText = article.categorie ?? 'Non spécifiée';
+
+const descriptionElement=document.createElement('div');
+descriptionElement.classList.add('text-gray-600', 'mb-2');
+descriptionElement.innerText = article.description ?? 'Aucune description disponible';
+
+const stautusElement=document.createElement('div');
+stautusElement.classList.add('text-gray-600', 'mb-2');
+stautusElement.innerHTML= '<span class="text-green-500 font-bold">En Stock</span>' ?? '<span class="text-red-500 font-extrabold">Indisponible</span>';
+
+const sectionFiche=document.querySelector('.fiches');
+const carte= document.createElement('div');
+carte.classList.add( 'bg-white', 'rounded-lg', 'shadow-md', 'p-4', 'mb-4');
+carte.appendChild(imageElement);
+carte.appendChild(titleElement);
+carte.appendChild(prixElement);
+carte.appendChild(categorieElement);
+carte.appendChild(descriptionElement);
+carte.appendChild(stautusElement);
+sectionFiche.classList.add('grid', 'grid-cols-1', 'md:grid-cols-2', 'lg:grid-cols-3', 'gap-4');
+sectionFiche.appendChild(carte);
+
+
+}
